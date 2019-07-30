a42e656d63ea386733f9868d4744b0c0be5c64ae
ethanzhu <764724624@qq.com>
2019-07-30 21:40:00 +0800

    添加微信聊天机器人

---
+++ everyday_wechat/install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/cloudswave/EverydayWechat
+cd EverydayWechat
+pip3 install -r requirements.txt
+# 或者是使用 pip
+# pip install -r requirements.txt
+# python run.py
+screen python run.py #可以使用screen让程序后台运行
