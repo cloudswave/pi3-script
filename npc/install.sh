1066f90b5694e35cd88ccc97b9c9fbdbf06c8006
root <root@raspbian>
2021-07-27 23:07:12 +0800

    add npc

---
+++ npc/install.sh
@@ -0,0 +1,8 @@
+sudo ./npc install -server=t2.ethanzhu.ga:8024 -vkey=rlq3ul14ihwvq1bz -type=tcp
+sudo npc start
+
+# https://ehang-io.github.io/nps/#/use?id=%e6%97%a0%e9%85%8d%e7%bd%ae%e6%96%87%e4%bb%b6%e6%a8%a1%e5%bc%8f
+#注册：sudo ./npc install 其他参数（例如-server=xx -vkey=xx或者-config=xxx）
+#启动：sudo npc start
+#停止：sudo npc stop
+#如果需要更换命令内容需要先卸载./npc uninstall，再重新注册
